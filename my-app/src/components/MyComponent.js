b833df3eaa2ba6b0ee8d21226c601b2a474b23fb
toniiwithJS <toan13022017@gmail.com>
2023-03-23 09:32:09 +0700

    setStage then re-render

--- my-app/src/components/MyComponent.js
+++ my-app/src/components/MyComponent.js
@@ -3,17 +3,21 @@ import React from 'react'; // nạp thư viện react
 class MyComponent extends React.Component { // tạo class Component
     // các biến, hàm khai báo trên render() không cần dùng let, const, var
     state = { // state được định nghĩa ở trên render()
+        name: 'Thanh Toan',
         today: `${new Date().getDate()} / ${new Date().getMonth()+1} / ${new Date().getFullYear()}`,
-        address: 'CAN THO'
-    }
+    }        
 
     handleClickButton = (logger) => {
         alert(logger);
-
+    }
+    
+    handleChangeName = (event) => {
+        this.setState({ // ghi đè lại thuộc tính name của state, các thuộc tính khác của state vẫn giữ nguyên 
+            name: event.target.value
+        })
     }
     
     render() {
-        let name = 'THANH TOAN';    
 
         return ( // HTML được viết trong "()"
             // <div className='block-container'> {/* #cách 1 block cha bọc 2 block con*/}
@@ -29,7 +33,7 @@ class MyComponent extends React.Component { // tạo class Component
 
             <> {/* #cách 2 dùng Fragment, cách này sẽ không có block cha nào bọc ở ngoài*/}
                 <div className='block-first'>
-                    This my Component, My name is: {`${name}, ${this.state.address}`} 
+                    This my Component, My name is: {this.state.name} 
                     {/* JS được viết trong "{}" */}
                 </div> 
                 <div className='block-first'>
@@ -44,6 +48,10 @@ class MyComponent extends React.Component { // tạo class Component
                     <button onClick={(logger) => this.handleClickButton('hello !')}> {/* dùng theo cú pháp này mới truyền tham số được */}
                         Say hello !
                     </button>
+                </div>
+                <div>
+                    You can change a new name
+                    <input onChange={(event) => this.handleChangeName(event)} value={this.state.name}/>
                 </div>
             </>
         )
